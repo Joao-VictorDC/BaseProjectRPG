b8e92a13914ac27d1a174ebdb9d3d9c5286a6cfd
Esdras Carneiro <esdras.aslc2@gmail.com>
2023-07-08 15:41:21 -0300

    Add menu system
    Add to patch a 2 menu
    - Main menu
    - Pause menu

---
+++ Pause_Menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+		$CenterContainer/VBoxContainer/ResumeBtn.grab_focus()
+
+
+var is_paused = false : 
+	set(value):
+		is_paused = value
+		get_tree().paused = is_paused
+		visible = is_paused
+
+
+
+func _on_resume_btn_pressed():
+	self.is_paused = false
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
---
+++ scenes/menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartBtn.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_btn_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main_Level.tscn")
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
