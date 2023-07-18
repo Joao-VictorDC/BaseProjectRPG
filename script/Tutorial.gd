f15fc3fc495ed5bcc9379799326fb73db1ddef49
João Victor Dórea Cupolo <64991598+Joao-VictorDC@users.noreply.github.com>
2023-07-17 22:33:29 -0300

    Level_design

--- script/Tutorial.gd
+++ script/Tutorial.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/BackBtn.grab_focus()
+	$HBoxContainer/Back_Button.grab_focus()
+	get_parent().
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
